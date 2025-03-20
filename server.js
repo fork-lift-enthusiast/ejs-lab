467afb448175e69fa514a7bf3d1098ad7682ecc8
bonnie <owu.owu.666@gmail.com>
2025-03-20 15:44:04 -0400

    more setup

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const express = require('express');
+import express from "express"
+import logger from "morgan"
+
 const app = express();
+
+app.use(logger("dev"))
 
 app.get('/', (req, res) => {
   res.send('Hello There!');
 });
 
-app.listen(3000);
+
+
+app.listen(3000, ()=>{
+    console.clear();
+    console.log("running on port 3000")
+});
